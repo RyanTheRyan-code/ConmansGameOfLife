5cc2ed15006efb32c3206ae3b3bf7191fd0ae81d
finadoggie <75299710+Finadoggie@users.noreply.github.com>
2024-10-18 18:50:29 -0700

    generate grid

--- main.js
+++ main.js
@@ -2,3 +2,21 @@ function DoSomething(that) {
     console.log("Wow");
     that.innerHTML =  ">.<";
 }
+
+function generateBoard(width=16, height=16) {
+    gameDiv = document.getElementById("game");
+    board = document.createElement("div");
+    board.className = "board";
+    for(i = 0; i < width; i++) {
+        line = document.createElement("div");
+        line.className = "line";
+        for (let j = 0; j < height; j++) {
+            space = document.createElement("div");
+            space.className = "space";
+            line.append(space);
+        }
+        board.append(line);
+    }
+    gameDiv.append(board);
+    console.log("done");
+}
