45dfd4f0d6587b201c868823430b361bfac40a21
Ryan Makela <rmmakela@csuchico.edu>
2024-10-18 19:01:46 -0700

    removed old code

--- main.js
+++ main.js
@@ -1,29 +1,4 @@
-conditions = [
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-    "ten",
-];
-
 boardExists = false;
-
-function DoSomething(that) {
-    console.log("Wow");
-    that.innerHTML =  ">.<";
-}
-
-function genCond(that) {
-    let i = Math.floor(Math.random() * 10);
-    console.log(i);
-    that.innerHTML = conditions[i];
-    console.log(conditions[i]);
-}
 
 function generateBoard(width=16, height=16) {
     if(boardExists) return;
