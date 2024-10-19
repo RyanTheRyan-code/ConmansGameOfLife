1aa783f64c695aaf0e39b7ff18464cd353976dc1
Ryan Makela <rmmakela@csuchico.edu>
2024-10-18 18:57:29 -0700

    generate conds

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
+conditions = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+];
+
 function DoSomething(that) {
     console.log("Wow");
     that.innerHTML =  ">.<";
+}
+
+function genCond(that) {
+    let i = Math.floor(Math.random() * 10);
+    console.log(i);
+    that.innerHTML = conditions[i];
+    console.log(conditions[i]);
 }
