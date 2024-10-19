79a66bba29360c5c9a339ba033c3b275b2f510d4
finadoggie <75299710+Finadoggie@users.noreply.github.com>
2024-10-18 19:18:32 -0700

    Implement changing grid space states

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ boardExists = false;
 
 function DoSomething(that) {
     console.log("Wow");
+    console.log(that);
     that.innerHTML =  ">.<";
 }
 
 function generateBoard(width=16, height=16) {
     if(boardExists) return;
     boardExists = true;
-    
+
     gameDiv = document.getElementById("game");
     board = document.createElement("div");
     board.className = "board";
@@ -17,11 +18,27 @@ function generateBoard(width=16, height=16) {
         line.className = "line";
         for (let j = 0; j < height; j++) {
             space = document.createElement("div");
-            space.className = "space";
+            space.classList.add("space");
+            space.id = `${i},${j}`;
+            space.onclick = function() { changeState(this); };
             line.append(space);
         }
         board.append(line);
     }
     gameDiv.append(board);
-    console.log("done");
+    console.log("finished generating board");
+}
+
+function changeState(space) {
+    console.log(space);
+    space.style.background = getRandomColor();
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
