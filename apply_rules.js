f694d5916bc6128d7f03aa526e026d1f33ccf1eb
RyanTheRyan-code <ryan.makela2004@gmail.com>
2024-10-19 06:56:12 -0700

    global current rules now

--- apply_rules.js
+++ apply_rules.js
@@ -2,7 +2,48 @@ const NO_STATE_CHANGE = -1;
 const DEAD_STATE = 0;
 const PROTECTED = 0.4;
 
+let currentRules = [ // conways by default
+    {type: 0, numCells: 2, comparison: 0, neighborState: 1},
+    {type: 1, numCells: 2, comparison: 2, neighborState: 1},
+    {type: 1, numCells: 3, comparison: 2, neighborState: 1},
+    {type: 0, numCells: 3, comparison: 4, neighborState: 1},
+    {type: 2, numCells: 3, comparison: 2, neighborState: 1, babyState: 1},
+];
 
+function addRandRule() {
+    let type = Math.floor(Math.random()*4);
+    switch(type) {
+        case 0:
+        case 1:
+            currentRules.push({
+                type: type,
+                numCells: Math.floor(Math.random()*9),
+                comparison: Math.floor(Math.random()*5),
+                neighborState: Math.ceil(Math.random()*4),
+            });
+            break;
+        case 2:
+            currentRules.push({
+                type: type,
+                numCells: Math.floor(Math.random()*9),
+                comparison: Math.floor(Math.random()*5),
+                neighborState: Math.ceil(Math.random()*4),
+                babyState: Math.ceil(Math.random()*4),
+            });
+            break;
+        case 3:
+            currentRules.push({
+                type: type,
+                numCells: Math.floor(Math.random()*9),
+                comparison: Math.floor(Math.random()*5),
+                neighborState: Math.ceil(Math.random()*4),
+                myState: Math.ceil(Math.random()*4),
+                babyState: Math.ceil(Math.random()*4),
+            });
+            break;
+        
+    }
+}
 
 // Any variables with `Map` at the end, are maps.
 // Any variables with `Coord` at the end, are [x,y] arrays.
@@ -12,35 +53,24 @@ const PROTECTED = 0.4;
 // returns: number checkAtNewState
 // params:  Map curStateMap, [number, number] checkAtCoord
 let applyRules = function(curStateMap, checkAtCoord) {
-    // let rules = [lessThanTwoAlive, twoOrThreeAlive, moreThanThreeAlive, exactThree];
-    // let rules = [lessThanTwoAlive, twoOrThreeAlive, moreThanThreeAlive, exactThree, fourToState2, oneToState2, threeS2ToState3];
-    let rules = [
-        {type: 0, numCells: 2, comparison: 0, neighborState: 1},
-        {type: 1, numCells: 2, comparison: 2, neighborState: 1},
-        {type: 1, numCells: 3, comparison: 2, neighborState: 1},
-        {type: 0, numCells: 3, comparison: 4, neighborState: 1},
-        {type: 2, numCells: 3, comparison: 2, neighborState: 1, babyState: 1}
-    ]
     let max = NO_STATE_CHANGE;
-    for(let rule of rules) {
-        // console.log(`${rule[0]}`)
+    for(let rule of currentRules) {
         let result = max;
         switch (rule.type) {
             case 0:
                 result = generalDead(rule.numCells, rule.comparison, rule.neighborState, curStateMap, checkAtCoord);
-                // console.log(`gendead on coord: ${checkAtCoord} and result: ${result}`);
                 break;
             case 1:
                 result = generalStay(rule.numCells, rule.comparison, rule.neighborState, curStateMap, checkAtCoord);
-                // console.log(`genstay and result: ${result}`);
                 break;
             case 2:
                 result = generalRepr(rule.numCells, rule.comparison, rule.neighborState, rule.babyState, curStateMap, checkAtCoord);
-                // console.log(`genrepr and result: ${result}`);
+                break;
+            case 3:
+                result = generalEvol(rule.numCells, rule.comparison, rule.neighborState, rule.myState, rule.babyState, curStateMap, checkAtCoord);
                 break;
         }
 
-        // console.log(`checking rule at ${checkAtCoord}, gets result ${result}`);
         max = (max > result) ? max : result;
     }
     return max;
@@ -104,69 +134,24 @@ let generalRepr = function(numCells, comparison, neighborState, babyState, curSt
     return NO_STATE_CHANGE;
 }
 
-// number lessThanTwoAlive(Map, [number, number])
-let lessThanTwoAlive = function (curStateMap, checkAtCoord) {
-    if (curStateMap.has(checkAtCoord.toString())) {
-        let sum = sumNeighbor(curStateMap, checkAtCoord, 1);
-        return (sum < 2) ? DEAD_STATE : NO_STATE_CHANGE;
-    }
-    return DEAD_STATE;
-}
-
-// number twoOrThreeAlive(Map, [number, number])
-let twoOrThreeAlive = function (curStateMap, checkAtCoord) {
-    if (curStateMap.has(checkAtCoord.toString())) {
-        let sum = sumNeighbor(curStateMap, checkAtCoord, 1);
-        // console.log(`two or three sum neighbors: ${sum}`);
-        return (sum == 2 || sum == 3) ? NO_STATE_CHANGE : DEAD_STATE;
-    }
-    return DEAD_STATE;
-}
-
-// number moreThanThreeAlive(Map, [number, number])
-let moreThanThreeAlive = function (curStateMap, checkAtCoord) {
-    if (curStateMap.has(checkAtCoord.toString())) {
-        let sum = sumNeighbor(curStateMap, checkAtCoord, 1);
-        return (sum > 3) ? DEAD_STATE : NO_STATE_CHANGE;
-    }
-    return DEAD_STATE;
-}
-
-// number exactThree(Map, [number, number])
-let exactThree = function (curStateMap, checkAtCoord) {
-    if (!curStateMap.has(checkAtCoord.toString())) {
-        // console.log(`come to life at ${checkAtCoord}?`);
-        let sum = sumNeighbor(curStateMap, checkAtCoord, 1);
-        // console.log(`sum ${sum}?`);
-        return (sum == 3) ? 1 : DEAD_STATE;
+let generalEvol = function(numCells, comparison, neighborState, myState, babyState, curStateMap, checkAtCoord) {
+    if (curStateMap.has(checkAtCoord.toString()) && curStateMap.get(checkAtCoord.toString()) == myState) {
+        let sum = sumNeighbor(curStateMap, checkAtCoord, neighborState);
+        switch (comparison) {
+            case 0:
+                return (sum < numCells) ? babyState : NO_STATE_CHANGE;
+            case 1:
+                return (sum <= numCells) ? babyState : NO_STATE_CHANGE;
+            case 2:
+                return (sum == numCells) ? babyState : NO_STATE_CHANGE;
+            case 3:
+                return (sum >= numCells) ? babyState : NO_STATE_CHANGE;
+            case 4:
+                return (sum > numCells) ? babyState : NO_STATE_CHANGE;
+        }
     }
     return NO_STATE_CHANGE;
 }
-
-let fourToState2 = function (curStateMap, checkAtCoord) {
-    if (!curStateMap.has(checkAtCoord.toString())) {
-        let sum = sumNeighbor(curStateMap, checkAtCoord, 1);
-        return (sum == 4) ? 2 : DEAD_STATE;
-    }
-    return NO_STATE_CHANGE;
-}
-
-let oneToState2 = function (curStateMap, checkAtCoord) {
-    if (curStateMap.has(checkAtCoord.toString())) {
-        let sum = sumNeighbor(curStateMap, checkAtCoord, 1);
-        return (sum == 1) ? 2 : DEAD_STATE;
-    }
-    return DEAD_STATE;
-}
-
-let threeS2ToState3 = function (curStateMap, checkAtCoord) {
-    if (curStateMap.has(checkAtCoord.toString())) {
-        let sum = sumNeighbor(curStateMap, checkAtCoord, 2);
-        return (sum >= 3) ? 3 : DEAD_STATE;
-    }
-    return DEAD_STATE;
-}
-
 
 
 // number sumNeighbor(Map, [number, number], number)
