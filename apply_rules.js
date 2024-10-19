bcf2c49ae2b48df11a95953ae7858ef615a7e037
IvanGav <ivan.gavby@gmail.com>
2024-10-18 22:56:11 -0700

    wip

--- apply_rules.js
+++ apply_rules.js
@@ -13,8 +13,38 @@ function step(prevStateMap) {
     let nextEmptyMap = new Map();
     prevStateMap.forEach((cellState, coord) => {
         //we could've already looked at it from its alive neighbors
-        if(!nextStateMap.has(coord) && !nextEmptyMap.has(coord)) {
-
-        }
+        if(!nextStateMap.has(coord) && !nextEmptyMap.has(coord)) { prevStateMap[coord] = applyRules(prevStateMap, coord); }
+        //look at neighbors
+        if(!nextStateMap.has(coord) && !nextEmptyMap.has(coord)) { prevStateMap[[coord[0]-1, coord[1]-1]] = applyRules(prevStateMap, [coord[0]-1, coord[1]-1]); }
+        if(!nextStateMap.has(coord) && !nextEmptyMap.has(coord)) { prevStateMap[[coord[0], coord[1]-1]] = applyRules(prevStateMap, [coord[0], coord[1]-1]); }
+        if(!nextStateMap.has(coord) && !nextEmptyMap.has(coord)) { prevStateMap[[coord[0]+1, coord[1]-1]] = applyRules(prevStateMap, [coord[0]+1, coord[1]-1]); }
+        if(!nextStateMap.has(coord) && !nextEmptyMap.has(coord)) { prevStateMap[[coord[0]-1, coord[1]-1]] = applyRules(prevStateMap, [coord[0]-1, coord[1]-1]); }
+        if(!nextStateMap.has(coord) && !nextEmptyMap.has(coord)) { prevStateMap[[coord[0]-1, coord[1]-1]] = applyRules(prevStateMap, [coord[0]-1, coord[1]-1]); }
+        if(!nextStateMap.has(coord) && !nextEmptyMap.has(coord)) { prevStateMap[[coord[0]-1, coord[1]-1]] = applyRules(prevStateMap, [coord[0]-1, coord[1]-1]); }
+        if(!nextStateMap.has(coord) && !nextEmptyMap.has(coord)) { prevStateMap[[coord[0]-1, coord[1]-1]] = applyRules(prevStateMap, [coord[0]-1, coord[1]-1]); }
+        if(!nextStateMap.has(coord) && !nextEmptyMap.has(coord)) { prevStateMap[[coord[0]-1, coord[1]-1]] = applyRules(prevStateMap, [coord[0]-1, coord[1]-1]); }
     });
 }
+
+// let rule1 = function(curStateMap, checkAtCoord) { 
+//     if(curStateMap[[checkAtCoord[0], checkAtCoord[1]+1]] == 1 || curStateMap[[checkAtCoord[0], checkAtCoord[1]-1]] == 1) {
+//         return 2;
+//     }
+//     return 0;
+// }
+// let rule2 = function(curStateMap, checkAtCoord) {
+//     if(curStateMap[[checkAtCoord[0]-1, checkAtCoord[1]]] == 1 || curStateMap[[checkAtCoord[0]+1, checkAtCoord[1]]] == 1) {
+//         return 3;
+//     }
+//     return 0;
+// }
+
+
+// applyRules = function(curStateMap, checkAtCoord) {
+//     let rules = [rule1, rule2];
+//     let m = 0;
+//     for(r in rules) {
+//         m = max(m, r(curStateMap, checkAtCoord));
+//     }
+//     return m;    
+// }
