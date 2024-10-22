f42514091f5e1d346e2fce33f981574b037cd3a3
ivangav <ivan.gavby@gmail.com>
2024-10-22 12:56:14 -0700

    added a copy button

--- dialog_modals.js
+++ dialog_modals.js
@@ -42,3 +42,14 @@ function dialogImportRules() {
     importRules(getImportSettings());
     closeDialogImport();
 }
+
+function copyExport() {
+    // Get the text field
+    var copyText = document.getElementById("exportSettingsOutput").value;
+    
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText);
+    
+    // Alert the copied text
+    alert("Copied!");
+}
